3f9d3966c34a7088ac4713c3c4f1a9ae807655a4
Richard Morley <richard@Richards-MBP.home>
2020-02-21 22:51:00 +0000

    Added fourth javascript project

---
+++ Task 4/scripts/rmorle01_jv_tma_task4.js
@@ -0,0 +1,51 @@
+function getTheDeepestLake() {
+    let lakes =
+        [
+            {
+                name: "Caspian Sea",
+                fathom: 56
+            },
+            {
+                name: "Tarn Hows",
+                fathom: 53
+            },
+            {
+                name: "Crater Lake",
+                fathom: 324
+            },
+            {
+                name: "Lake Tanganyika",
+                fathom: 803
+            },
+            {
+                name: "Lake Vostok",
+                fathom: 546
+            },
+            {
+                name: "Lake Baikal",
+                fathom: 897
+            }
+        ];
+
+    return lakes.reduce(function (a, b) {
+        return a.fathom > b.fathom ? a : b
+    });
+}
+
+function convertFathomToMetre() {
+    let fathom = getTheDeepestLake().fathom;
+    let convertToMetre = fathom * 1.829;
+    return convertToMetre.toFixed(2);
+}
+
+function printDeepestLake() {
+    console.log("The deepest lake is: " + getTheDeepestLake().name);
+    console.log("This lake is " + convertFathomToMetre() + "m deep.");
+}
+
+printDeepestLake();
+
+
+
+
+
